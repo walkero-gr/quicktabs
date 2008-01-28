f9d43b77631a267bc62ccfbe0c96ebb06adfe9bf
Katherine Bailey <katbailey@172987.no-reply.drupal.org>
2008-01-28 18:44:53 +0000

    And now for the missing js files

---
+++ js/quicktabs.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('div.quicktabs').hide();
+  $('div.quicktabs:first-child').show();
+    $('ul.quicktabs_tabs li:first-child').addClass('active');
+  
+  var clickFunction = function() {      
+    var tabIndex = this.myTabIndex;
+    $(this).parent().parent().parent().find('div.quicktabs').hide();
+    $(this).parent().siblings().removeClass('active');
+    $(this).parent().addClass('active');
+    $(this).parent().parent().parent().find('div.quicktabs:eq('+tabIndex+')').show();
+    return false;
+  };
+  $('ul.quicktabs_tabs').each(function(){
+    var i = 0;
+    $(this).find('li a').each(function() {
+      this.myTabIndex = i++;
+    });
+  });
+  $('ul.quicktabs_tabs li a').bind('click', clickFunction);
+});
---
+++ js/quicktabs_form.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('#quicktabs-viewsform div.form-item :input[@name^="bid"]').parent().hide();
+  var showhide = function() {
+    if (this.value == 'block') {
+      $(this).parent().parent()
+      .find('div.form-item :input[@name^="vid"]').parent().hide();
+      $(this).parent().parent()
+      .find('div.form-item :input[@name^="args"]').parent().hide();
+      $(this).parent().parent()
+      .find('div.form-item :input[@name^="limit"]').parent().hide();
+      $(this).parent().parent()
+      .find('div.form-item :input[@name^="bid"]').parent().show();    
+   } else {
+      $(this).parent().parent()
+      .find('div.form-item :input[@name^="bid"]').parent().hide();      
+      $(this).parent().parent()
+      .find('div.form-item :input[@name^="vid"]').parent().show();
+      $(this).parent().parent()
+      .find('div.form-item :input[@name^="args"]').parent().show();
+      $(this).parent().parent()
+      .find('div.form-item :input[@name^="limit"]').parent().show();  
+   }
+  };
+  $('#quicktabs-viewsform div.form-item :input[@name^="type"]').bind('change', showhide);
+});
