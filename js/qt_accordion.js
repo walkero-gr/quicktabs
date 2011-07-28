8bd71f8d66716b88182af4b6e9032cc400a35021
Katherine Bailey <katherine@katbailey.net>
2011-07-28 14:55:32 -0700

    Issue #1231496: Accordion problems still, ugh

--- js/qt_accordion.js
+++ js/qt_accordion.js
@@ -3,7 +3,7 @@
 Drupal.behaviors.qt_accordion = {
   attach: function (context, settings) {
     $('.quick-accordion').once(function(){
-      var qtKey = 'qt_' + Drupal.quicktabs.getQTName(this);
+      var qtKey = 'qt_' + this.id.substring(this.id.indexOf('-') +1);
       var options = Drupal.settings.quicktabs[qtKey].options;
       var active_tab = parseInt(Drupal.settings.quicktabs[qtKey].active_tab);
       options.active = active_tab;
