06c6c7dd3afaf2a53603d8881f6283d433cf25fc
Palash VIjay <palashvijay13319@gmail.com>
2015-07-27 05:17:59 +0530

    Added annotation

---
+++ src/Annotation/QuicktabFormat.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\quicktabs\Annotation\QuicktabFormat
+ */
+namespace Drupal\quicktabs\Annotation;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Defines an quicktab annotation object.
+ */
+class QuicktabFormat extends Plugin {
+
+  /**
+   * The plugin ID.
+   *
+   * @var string
+   */
+  public $id;
+}
