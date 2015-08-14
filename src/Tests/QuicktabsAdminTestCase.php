7d9fbeb9fae264795901cf6e7f21db6bb1750c3a
Palash VIjay <palashvijay13319@gmail.com>
2015-08-14 22:11:52 +0530

    Added stub test case file

---
+++ src/Tests/QuicktabsAdminTestCase.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\quicktabs\Tests\QuicktabsAdminTestCase.php.
+ */
+
+namespace Drupal\quicktabs\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Add, edit and delete quicktabs.
+ * @ingroup Quicktabs
+ * @group Quicktabs
+ */
+class QuicktabsAdminTestCase extends WebTestBase {
+
+}
