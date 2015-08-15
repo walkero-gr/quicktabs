825fcbe104ea59440c65d7ea30f20106f3aab380
Palash VIjay <palashvijay13319@gmail.com>
2015-08-15 21:48:12 +0530

    Creating 5 random nodes for testing

--- src/Tests/QuicktabsAdminTestCase.php
+++ src/Tests/QuicktabsAdminTestCase.php
@@ -7,6 +7,7 @@
 namespace Drupal\quicktabs\Tests;
 
 use Drupal\node\Tests\NodeTestBase;
+use Drupal\Core\Language\Language;
 
 /**
  * Add, edit and delete quicktabs.
@@ -27,6 +28,15 @@ class QuicktabsAdminTestCase extends NodeTestBase {
 
     $admin_user = $this->drupalCreateUser(array('access administration pages', 'administer quicktabs', 'administer nodes'));
     $this->drupalLogin($admin_user);
+
+    for ($i = 0; $i < 5; $i++) {
+      $node = new \stdClass();
+      $node->type = 'page';
+      $node->title = 'This is node number '. ($i+1);
+      $node->body[Language::LANGCODE_NOT_SPECIFIED][0]['value'] = $this->randomString(255);
+      node_object_prepare($node);
+      node_save($node);
+    }
   }
 
 
